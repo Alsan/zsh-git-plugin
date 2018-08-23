a21c11cf5f8fde62d104061eb707c4bef8057914
alsan <alsan.wong@gmail.com>
2018-08-23 09:05:01 +0800

    add jdk home export

--- sdkman.zsh
+++ sdkman.zsh
@@ -1,5 +1,6 @@
 export SDKMAN_DIR="$HOME/.sdkman"
 export JAVA_HOME="$SDKMAN_DIR/java/candidates/current"
+export JDK_HOME=$JAVA_HOME
 export GRADLE_HOME="$SDKMAN_DIR/gradle/candidates/current"
 export PATH=$PATH:$JAVA_HOME/bin:$GRADLE_HOME/bin
 
